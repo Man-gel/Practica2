f95c7c8d3060fd59e537126417cb6f127e52919f
javieroropeza <night_captain@hotmail.com>
2014-10-02 19:46:12 -0500

    Código sin static y con un objeto Producto(Mejorado)

---
+++ Practica2/Practica2/Productos.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 02/10/2014
+ * Time: 07:39 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Practica2
+{
+	/// <summary>
+	/// Description of Productos.
+	/// </summary>
+	public class Productos
+	{
+		public Productos()
+		{
+		}
+		
+		private int cantidad1  = 0;
+		private int cantidad2  = 0;
+		private int cantidad3  = 0;
+		private string descripcion1 = "";
+		private string descripcion2 = "";
+		private string descripcion3 = "";
+		private double precio1 = 0;
+		private double precio2 = 0;
+		private double precio3 = 0;
+		
+		public void capturarTresProductos(){
+			this.cantidad1 = asignarCantidad();
+			this.descripcion1 = asignarDescripcion();
+			this.precio1 = asignarPrecio();
+			this.cantidad2 = asignarCantidad();
+			this.descripcion2 = asignarDescripcion();
+			this.precio2 = asignarPrecio();
+			this.cantidad3 = asignarCantidad();
+			this.descripcion3 = asignarDescripcion();
+			this.precio3 = asignarPrecio();
+			mostrarNota();
+		}
+		
+		
+		private int asignarCantidad()
+		{
+			Console.WriteLine();
+			Console.WriteLine("Ingresa la cantidad del producto:");
+			int cantidad = Int32.Parse( Console.ReadLine()  );
+			return cantidad; 
+		}
+		
+		private string asignarDescripcion()
+		{
+			Console.WriteLine("Ingresa la descripcion del producto:");
+			string descripcion = Console.ReadLine();
+			return descripcion; 
+		}
+		
+		private double asignarPrecio()
+		{
+			Console.Write("Ingresa el precio unitario del producto: $");
+			//Console.ReadKey(true);
+			double precio = double.Parse( Console.ReadLine() );
+			return precio; 
+		}
+		
+		private void mostrarNota()
+		{
+			double subtotal1 = cantidad1*precio1;
+			double subtotal2 = cantidad2*precio2;
+			double subtotal3 = cantidad3*precio3;
+			
+			Console.WriteLine();
+			Console.WriteLine("CANTIDAD DESCRIPCION     PRECIO ");
+			Console.WriteLine();
+			Console.WriteLine(" "+cantidad1+"    "+descripcion1+"      "+subtotal1);
+			Console.WriteLine(" "+cantidad2+"    "+descripcion2+"      "+subtotal2);
+			Console.WriteLine(" "+cantidad3+"    "+descripcion3+"      "+subtotal3);
+			calcularIVA(subtotal1,subtotal2,subtotal3);
+			Console.ReadKey(true);
+		}
+		
+		private void calcularIVA(double subt1,double subt2,double subt3)
+		{
+			double subTotal = subt1+subt2+subt3;
+			double iva = subTotal * 0.16;
+			double total = subTotal + iva;
+			Console.WriteLine("SUBTOTAL: $"+ subTotal);
+			Console.WriteLine("IVA: $"+ iva);
+			Console.WriteLine("TOTAL: $"+ total);
+		}
+		
+	}
+}
--- Practica2/Practica2/Program.cs
+++ Practica2/Practica2/Program.cs
@@ -12,84 +12,13 @@ namespace Practica2
 {
 	class Program
 	{
-		private static int cantidad1  = 0;
-		private static int cantidad2  = 0;
-		private static int cantidad3  = 0;
-		private static string descripcion1 = "";
-		private static string descripcion2 = "";
-		private static string descripcion3 = "";
-		private static double precio1 = 0;
-		private static double precio2 = 0;
-		private static double precio3 = 0;
-		
-		
-		public  static int asignarCantidad()
-		{
-			Console.WriteLine();
-			Console.WriteLine("Ingresa la cantidad del producto:");
-			int cantidad = Int32.Parse( Console.ReadLine()  );
-			return cantidad; 
-		}
-		
-		public  static string asignarDescripcion()
-		{
-			Console.WriteLine("Ingresa la descripcion del producto:");
-			string descripcion = Console.ReadLine();
-			return descripcion; 
-		}
-		
-		public  static double asignarPrecio()
-		{
-			Console.Write("Ingresa el precio unitario del producto: $");
-			//Console.ReadKey(true);
-			double precio = double.Parse( Console.ReadLine() );
-			return precio; 
-		}
-		
-		public static void mostrarNota()
-		{
-		double subtotal1 = cantidad1*precio1;
-		double subtotal2 = cantidad2*precio2;
-		double subtotal3 = cantidad3*precio3; 
-			Console.WriteLine("CANTIDAD DESCRIPCION     PRECIO ");
-			Console.WriteLine();
-			Console.WriteLine(" "+cantidad1+"    "+descripcion1+"      "+subtotal1);
-			Console.WriteLine(" "+cantidad2+"    "+descripcion2+"      "+subtotal2);
-			Console.WriteLine(" "+cantidad3+"    "+descripcion3+"      "+subtotal3);
-			calcularIVA(subtotal1,subtotal2,subtotal3);
-			Console.ReadKey(true);
-		}
-		
-		private static void calcularIVA(double subt1,double subt2,double subt3)
-		{
-			double subTotal = subt1+subt2+subt3;
-			double iva = subTotal * 0.16;
-			double total = subTotal + iva;
-			Console.WriteLine("SUBTOTAL: "+ subTotal);
-			Console.WriteLine("IVA: "+ iva);
-			Console.WriteLine("TOTAL: "+ total);
-			
-		}
 		
 		
 		
 		public static void Main(string[] args)
 		{
-			cantidad1 = asignarCantidad();
-			descripcion1 = asignarDescripcion();
-			precio1 = asignarPrecio();
-			cantidad2 = asignarCantidad();
-			descripcion2 = asignarDescripcion();
-			precio2 = asignarPrecio();
-			cantidad3 = asignarCantidad();
-			descripcion3 = asignarDescripcion();
-			precio3 = asignarPrecio();
-			mostrarNota();
-			
-			//Console.WriteLine("Prd1: "+descripcion1+" cant: "+cantidad1+" Precio: "+precio1);
-			//Console.WriteLine("Prd2: "+descripcion2+" cant: "+cantidad2+" Precio: "+precio2);
-			//Console.WriteLine("Prd3: "+descripcion3+" cant: "+cantidad3+" Precio: "+precio3);
-			//Console.ReadKey(true);
+			Productos producto = new Productos();
+			producto.capturarTresProductos();
 			
 		}
 	}
